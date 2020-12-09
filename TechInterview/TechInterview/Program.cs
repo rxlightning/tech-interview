8104eda994862918ec49d214b5fa4385f0d079ef
Caitlin Wood <caitlinwood@Caitlins-MacBook-Pro.local>
2020-12-09 10:01:18 -0600

    init project and basic input scan

---
+++ TechInterview/TechInterview/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace TechInterview
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("Please enter a time:");
+
+                var time = Console.ReadLine();
+                
+                Console.WriteLine("Output:");
+                Console.WriteLine("clock representation here");
+            }
+        }
+    }
+}
